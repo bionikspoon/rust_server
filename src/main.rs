0f4f93cb10ce16a3818ab13cd79c1b9bbf45f6e0
Manu Phatak <manu@thinkcerca.com>
2017-11-12 15:43:41 -0600

    send index.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
@@ -15,10 +16,18 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
 
     stream.read(&mut buffer).unwrap();
-    println!("==========", );
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let mut file = File::open("index.html").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+
+    println!("<<<<<<<<<<");
+    println!("{}", String::from_utf8_lossy(&buffer[..]));
+    println!("==========");
+    println!("{}", response);
+    println!(">>>>>>>>>>");
 }
